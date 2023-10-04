ebfd53fc9d1cd53591c299b89499b9d0d756bb45
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-10-03 23:24:28 -0400

    Fin étape 15

--- Business/CoursesManager.cs
+++ Business/CoursesManager.cs
@@ -8,4 +8,12 @@ using System.Threading.Tasks;
 namespace _420DW3_Exercice_Intra_Test.Business;
 public class CoursesManager {
     private CoursesForm form;
+
+    public CoursesManager() {
+        this.form = new CoursesForm(this);
+    }
+
+    public void OpenCoursesWindow() {
+        DialogResult result = this.form.ShowDialog();
+    }
 }
