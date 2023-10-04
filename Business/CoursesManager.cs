7707734e2f3a3d44c3f84c68276db99a04a209e0
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-10-03 23:07:57 -0400

    Fin étape 13

---
+++ Business/CoursesManager.cs
@@ -0,0 +1,11 @@
+﻿using _420DW3_Exercice_Intra_Test.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DW3_Exercice_Intra_Test.Business;
+public class CoursesManager {
+    private CoursesForm form;
+}
