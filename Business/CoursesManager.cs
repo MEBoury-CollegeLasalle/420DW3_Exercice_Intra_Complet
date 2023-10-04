7cf6b2c82df28322137032accde005267b2eb6a1
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-10-04 01:22:50 -0400

    fin étape 28

--- Business/CoursesManager.cs
+++ Business/CoursesManager.cs
@@ -3,6 +3,7 @@ using _420DW3_Exercice_Intra_Test.GUI;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,10 @@ namespace _420DW3_Exercice_Intra_Test.Business;
 public class CoursesManager {
     private CoursesForm form;
     private CoursesDAO courseDAO;
+    private DataSet dataSet;
 
-    public CoursesManager(SqlConnection connection) {
+    public CoursesManager(SqlConnection connection, DataSet dataSet) {
+        this.dataSet = dataSet;
         this.form = new CoursesForm(this);
         this.courseDAO = new CoursesDAO(connection);
     }
